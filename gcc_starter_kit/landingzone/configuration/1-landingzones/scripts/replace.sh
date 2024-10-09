724db1136b5d953c7f249d77619fd0eee44261b6
thiamsoontan <thiamsoontan@microsoft.com>
2024-10-09 08:57:46 +0800

    Update replace.sh

--- gcc_starter_kit/landingzone/configuration/1-landingzones/scripts/replace.sh
+++ gcc_starter_kit/landingzone/configuration/1-landingzones/scripts/replace.sh
@@ -55,7 +55,8 @@ configure_nsg () {
 #------------------------------------------------------------------------
 
 
-CONFIG_FILE_PATH="/tf/avm/gcc_starter_kit/landingzone/configuration/0-launchpad/scripts/config.yaml"
+# CONFIG_FILE_PATH="/tf/avm/gcc_starter_kit/landingzone/configuration/0-launchpad/scripts/config.yaml"
+CONFIG_FILE_PATH="./../../0-launchpad/scripts/config.yaml"
 echo $CONFIG_FILE_PATH
 eval $(parse_yaml $CONFIG_FILE_PATH "CONFIG_")
 # e,g,
