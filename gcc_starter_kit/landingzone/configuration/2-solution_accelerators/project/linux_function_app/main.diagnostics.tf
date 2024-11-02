16356d0792f34fc79549a21558bccf48f35b9b8a
thiamsoontan <thiamsoontan@microsoft.com>
2024-11-02 23:11:07 +0800

    Update main.diagnostics.tf

--- gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/linux_function_app/main.diagnostics.tf
+++ gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/linux_function_app/main.diagnostics.tf
@@ -1,7 +1,7 @@
 module "diagnosticsetting1" {
   source = "AcceleratorFramew0rk/aaf/azurerm//modules/diagnostics/terraform-azurerm-diagnosticsetting"  
 
-  name                = "${module.naming.monitor_diagnostic_setting.name_unique}-kv"
+  name                = "${module.naming.monitor_diagnostic_setting.name_unique}-funcapp"
   target_resource_id = module.linux_function_app.resource.id
   log_analytics_workspace_id = try(local.remote.log_analytics_workspace.id, null) != null ? local.remote.log_analytics_workspace.id : var.log_analytics_workspace_id
   diagnostics = {
