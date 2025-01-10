d6b73d5b8e423cf60d12a05f806cff5405977730
thiamsoontan <thiamsoontan@microsoft.com>
2025-01-10 15:10:50 +0800

    remove hardcode of variables

--- gcc_starter_kit/landingzone/configuration/2-solution_accelerators/deploy_pattern_appservice_internet_intranet_architype.sh
+++ gcc_starter_kit/landingzone/configuration/2-solution_accelerators/deploy_pattern_appservice_internet_intranet_architype.sh
@@ -3,101 +3,101 @@
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/keyvault
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=nirxuat-rg-launchpad" \
--backend-config="storage_account_name=nirxuatstgtfstatedxh" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-keyvault.tfstate"
 
 terraform plan \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 # app service internet
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/app_service
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=nirxuat-rg-launchpad" \
--backend-config="storage_account_name=nirxuatstgtfstatedxh" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-appservice.tfstate"
 
 terraform plan \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 # app service intranet
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/app_service_intranet
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=nirxuat-rg-launchpad" \
--backend-config="storage_account_name=nirxuatstgtfstatedxh" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-appserviceintranet.tfstate"
 
 terraform plan \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 # cosmos db
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/cosmos_db_mongo
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=nirxuat-rg-launchpad" \
--backend-config="storage_account_name=nirxuatstgtfstatedxh" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-cosmosdb.tfstate"
 
 terraform plan \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 # # mssql
 # cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/mssql
 
 # terraform init  -reconfigure \
-# -backend-config="resource_group_name=nirxuat-rg-launchpad" \
-# -backend-config="storage_account_name=nirxuatstgtfstatedxh" \
+# -backend-config="resource_group_name={{resource_group_name}}" \
+# -backend-config="storage_account_name={{storage_account_name}}" \
 # -backend-config="container_name=2-solution-accelerators" \
 # -backend-config="key=solution_accelerators-project-mssql.tfstate"
 
 # terraform plan \
-# -var="storage_account_name=nirxuatstgtfstatedxh" \
-# -var="resource_group_name=nirxuat-rg-launchpad"
+# -var="storage_account_name={{storage_account_name}}" \
+# -var="resource_group_name={{resource_group_name}}"
 
 # terraform apply -auto-approve \
-# -var="storage_account_name=nirxuatstgtfstatedxh" \
-# -var="resource_group_name=nirxuat-rg-launchpad"
+# -var="storage_account_name={{storage_account_name}}" \
+# -var="resource_group_name={{resource_group_name}}"
 
 
 # storage account
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/storage_account
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=nirxuat-rg-launchpad" \
--backend-config="storage_account_name=nirxuatstgtfstatedxh" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-storageaccount.tfstate"
 
 terraform plan \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
--- gcc_starter_kit/landingzone/configuration/2-solution_accelerators/deploy_pattern_iot_archetype.sh
+++ gcc_starter_kit/landingzone/configuration/2-solution_accelerators/deploy_pattern_iot_archetype.sh
@@ -3,140 +3,140 @@
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/keyvault
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=nirxuat-rg-launchpad" \
--backend-config="storage_account_name=nirxuatstgtfstatedxh" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-keyvault.tfstate"
 
 terraform plan \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 # app service
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/app_service
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=nirxuat-rg-launchpad" \
--backend-config="storage_account_name=nirxuatstgtfstatedxh" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-appservice.tfstate"
 
 terraform plan \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 # mssql
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/mssql
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=nirxuat-rg-launchpad" \
--backend-config="storage_account_name=nirxuatstgtfstatedxh" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-mssql.tfstate"
 
 terraform plan \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 
 # storage account
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/storage_account
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=nirxuat-rg-launchpad" \
--backend-config="storage_account_name=nirxuatstgtfstatedxh" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-storageaccount.tfstate"
 
 terraform plan \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 # apim
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/apim
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=nirxuat-rg-launchpad" \
--backend-config="storage_account_name=nirxuatstgtfstatedxh" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-apim.tfstate"
 
 terraform plan \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 
 # linux function app
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/linux_function_app
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=nirxuat-rg-launchpad" \
--backend-config="storage_account_name=nirxuatstgtfstatedxh" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-linuxfunctionapp.tfstate"
 
 terraform plan \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 
 # iot hub
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/iot_hub
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=uatlite-rg-launchpad" \
--backend-config="storage_account_name=uatlitestgtfstate3a9" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-iothub.tfstate"
 
 terraform plan \
--var="storage_account_name=uatlitestgtfstate3a9" \
--var="resource_group_name=uatlite-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=uatlitestgtfstate3a9" \
--var="resource_group_name=uatlite-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 # event hubs
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/event_hubs
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=uatlite-rg-launchpad" \
--backend-config="storage_account_name=uatlitestgtfstate3a9" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-eventhubs.tfstate"
 
 terraform plan \
--var="storage_account_name=uatlitestgtfstate3a9" \
--var="resource_group_name=uatlite-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=uatlitestgtfstate3a9" \
--var="resource_group_name=uatlite-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 
 
@@ -144,19 +144,19 @@ terraform apply -auto-approve \
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/data_explorer
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=uatlite-rg-launchpad" \
--backend-config="storage_account_name=uatlitestgtfstate3a9" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-dataexplorer.tfstate"
 
 
 terraform plan \
--var="storage_account_name=uatlitestgtfstate3a9" \
--var="resource_group_name=uatlite-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=uatlitestgtfstate3a9" \
--var="resource_group_name=uatlite-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 
 
@@ -164,18 +164,18 @@ terraform apply -auto-approve \
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/vm
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=nirxuat-rg-launchpad" \
--backend-config="storage_account_name=nirxuatstgtfstatedxh" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-vm.tfstate"
 
 terraform plan \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=nirxuatstgtfstatedxh" \
--var="resource_group_name=nirxuat-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 
 
@@ -184,18 +184,18 @@ terraform apply -auto-approve \
 cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/stream_analytics
 
 terraform init  -reconfigure \
--backend-config="resource_group_name=uatlite-rg-launchpad" \
--backend-config="storage_account_name=uatlitestgtfstate3a9" \
+-backend-config="resource_group_name={{resource_group_name}}" \
+-backend-config="storage_account_name={{storage_account_name}}" \
 -backend-config="container_name=2-solution-accelerators" \
 -backend-config="key=solution_accelerators-project-streamanalytics.tfstate"
 
 terraform plan \
--var="storage_account_name=uatlitestgtfstate3a9" \
--var="resource_group_name=uatlite-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 terraform apply -auto-approve \
--var="storage_account_name=uatlitestgtfstate3a9" \
--var="resource_group_name=uatlite-rg-launchpad"
+-var="storage_account_name={{storage_account_name}}" \
+-var="resource_group_name={{resource_group_name}}"
 
 # # Approved via Azure CLI
 # az network private-endpoint-connection approve \
