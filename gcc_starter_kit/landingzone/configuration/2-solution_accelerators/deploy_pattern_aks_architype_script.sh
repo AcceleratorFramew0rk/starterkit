99e875353ca23991a2a5fda4c8aae391fd277793
thiamsoontan <thiamsoontan@microsoft.com>
2025-01-13 11:40:58 +0800

    add automation script for app service and aks archetype

---
+++ gcc_starter_kit/landingzone/configuration/2-solution_accelerators/deploy_pattern_aks_architype_script.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------
+# functions
+#------------------------------------------------------------------------
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+#------------------------------------------------------------------------
+# get configuration file path, resource group name, storage account name, subscription id, subscription name
+#------------------------------------------------------------------------
+CONFIG_FILE_PATH="/tf/avm/gcc_starter_kit/landingzone/configuration/0-launchpad/scripts/config.yaml"
+echo $CONFIG_FILE_PATH
+eval $(parse_yaml $CONFIG_FILE_PATH "CONFIG_")
+# Define your variables
+PROJECT_CODE="${CONFIG_prefix}" 
+PREFIX="${CONFIG_prefix}" 
+# Generate resource group name to store state file
+RG_NAME="${PROJECT_CODE}-rg-launchpad"
+
+STORAGE_ACCOUNT_NAME_PREFIX="${PROJECT_CODE}stgtfstate"
+STORAGE_ACCOUNT_INFO=$(az storage account list --resource-group $RG_NAME --query "[?contains(name, '$STORAGE_ACCOUNT_NAME_PREFIX')]" 2> /dev/null)
+if [[ $? -ne 0 ]]; then
+    echo "no storage account"
+    exit
+else
+    STG_NAME=$(echo "$STORAGE_ACCOUNT_INFO" | jq ".[0].name" -r)
+    ACCOUNT_INFO=$(az account show 2> /dev/null)
+    if [[ $? -ne 0 ]]; then
+        echo "no subscription"
+        exit
+    fi
+    SUB_ID=$(echo "$ACCOUNT_INFO" | jq ".id" -r)
+    SUB_NAME=$(echo "$ACCOUNT_INFO" | jq ".name" -r)
+    USER_NAME=$(echo "$ACCOUNT_INFO" | jq ".user.name" -r)
+    SUBSCRIPTION_ID="${SUB_ID}" 
+
+fi
+#------------------------------------------------------------------------
+# end get configuration file path, resource group name, storage account name, subscription id, subscription name
+#------------------------------------------------------------------------
+
+
+echo "PREFIX: ${PREFIX}"
+echo "Subscription Id: ${SUB_ID}"
+echo "Subscription Name: ${SUB_NAME}"
+echo "Storage Account Name: ${STG_NAME}"
+echo "Resource Group Name: ${RG_NAME}"
+
+# keyvault - is the first resource to be deployed in the solution accelerators. 
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/keyvault
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-keyvault.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+# acr
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/acr
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-acr.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+# aks
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/aks_avm_ptn
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-aks.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+# mssql
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/mssql
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-mssql.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+
+# storage account
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/storage_account
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-storageaccount.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
