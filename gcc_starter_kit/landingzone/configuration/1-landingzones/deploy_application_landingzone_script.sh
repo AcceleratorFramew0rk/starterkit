33f8f06571eb455d0e8280e5024be96fcf5e94ae
thiamsoontan <thiamsoontan@microsoft.com>
2025-01-13 10:51:02 +0800

    add automation script for iot archetype

---
+++ gcc_starter_kit/landingzone/configuration/1-landingzones/deploy_application_landingzone_script.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------
+# functions
+#------------------------------------------------------------------------
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+#------------------------------------------------------------------------
+# get configuration file path, resource group name, storage account name, subscription id, subscription name
+#------------------------------------------------------------------------
+CONFIG_FILE_PATH="/tf/avm/gcc_starter_kit/landingzone/configuration/0-launchpad/scripts/config.yaml"
+echo $CONFIG_FILE_PATH
+eval $(parse_yaml $CONFIG_FILE_PATH "CONFIG_")
+# Define your variables
+PROJECT_CODE="${CONFIG_prefix}" 
+PREFIX="${CONFIG_prefix}" 
+# Generate resource group name to store state file
+RG_NAME="${PROJECT_CODE}-rg-launchpad"
+
+STORAGE_ACCOUNT_NAME_PREFIX="${PROJECT_CODE}stgtfstate"
+STORAGE_ACCOUNT_INFO=$(az storage account list --resource-group $RG_NAME --query "[?contains(name, '$STORAGE_ACCOUNT_NAME_PREFIX')]" 2> /dev/null)
+if [[ $? -ne 0 ]]; then
+    echo "no storage account"
+    exit
+else
+    STG_NAME=$(echo "$STORAGE_ACCOUNT_INFO" | jq ".[0].name" -r)
+    ACCOUNT_INFO=$(az account show 2> /dev/null)
+    if [[ $? -ne 0 ]]; then
+        echo "no subscription"
+        exit
+    fi
+    SUB_ID=$(echo "$ACCOUNT_INFO" | jq ".id" -r)
+    SUB_NAME=$(echo "$ACCOUNT_INFO" | jq ".name" -r)
+    USER_NAME=$(echo "$ACCOUNT_INFO" | jq ".user.name" -r)
+    SUBSCRIPTION_ID="${SUB_ID}" 
+
+fi
+#------------------------------------------------------------------------
+# end get configuration file path, resource group name, storage account name, subscription id, subscription name
+#------------------------------------------------------------------------
+
+
+echo "PREFIX: ${PREFIX}"
+echo "Subscription Id: ${SUB_ID}"
+echo "Subscription Name: ${SUB_NAME}"
+echo "Storage Account Name: ${STG_NAME}"
+echo "Resource Group Name: ${RG_NAME}"
+
+
+
+# spoke project 
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/1-landingzones/application/networking_spoke_project
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=1-landingzones" \
+-backend-config="key=network-spoke-project.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+# spoke devops 
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/1-landingzones/application/networking_spoke_devops
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=1-landingzones" \
+-backend-config="key=network-spoke-devops.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+# peering project-devops
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/1-landingzones/application/networking_peering_project_devops
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=1-landingzones" \
+-backend-config="key=network-peering-project-devops.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
---
+++ gcc_starter_kit/landingzone/configuration/2-solution_accelerators/deploy_pattern_iot_archetype_script.sh
@@ -0,0 +1,263 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------
+# functions
+#------------------------------------------------------------------------
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+#------------------------------------------------------------------------
+# get configuration file path, resource group name, storage account name, subscription id, subscription name
+#------------------------------------------------------------------------
+CONFIG_FILE_PATH="/tf/avm/gcc_starter_kit/landingzone/configuration/0-launchpad/scripts/config.yaml"
+echo $CONFIG_FILE_PATH
+eval $(parse_yaml $CONFIG_FILE_PATH "CONFIG_")
+# Define your variables
+PROJECT_CODE="${CONFIG_prefix}" 
+PREFIX="${CONFIG_prefix}" 
+# Generate resource group name to store state file
+RG_NAME="${PROJECT_CODE}-rg-launchpad"
+
+STORAGE_ACCOUNT_NAME_PREFIX="${PROJECT_CODE}stgtfstate"
+STORAGE_ACCOUNT_INFO=$(az storage account list --resource-group $RG_NAME --query "[?contains(name, '$STORAGE_ACCOUNT_NAME_PREFIX')]" 2> /dev/null)
+if [[ $? -ne 0 ]]; then
+    echo "no storage account"
+    exit
+else
+    STG_NAME=$(echo "$STORAGE_ACCOUNT_INFO" | jq ".[0].name" -r)
+    ACCOUNT_INFO=$(az account show 2> /dev/null)
+    if [[ $? -ne 0 ]]; then
+        echo "no subscription"
+        exit
+    fi
+    SUB_ID=$(echo "$ACCOUNT_INFO" | jq ".id" -r)
+    SUB_NAME=$(echo "$ACCOUNT_INFO" | jq ".name" -r)
+    USER_NAME=$(echo "$ACCOUNT_INFO" | jq ".user.name" -r)
+    SUBSCRIPTION_ID="${SUB_ID}" 
+
+fi
+#------------------------------------------------------------------------
+# end get configuration file path, resource group name, storage account name, subscription id, subscription name
+#------------------------------------------------------------------------
+
+
+echo "PREFIX: ${PREFIX}"
+echo "Subscription Id: ${SUB_ID}"
+echo "Subscription Name: ${SUB_NAME}"
+echo "Storage Account Name: ${STG_NAME}"
+echo "Resource Group Name: ${RG_NAME}"
+
+
+# keyvault
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/keyvault
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-keyvault.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+# app service
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/app_service
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-appservice.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+# mssql
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/mssql
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-mssql.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+
+# storage account
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/storage_account
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-storageaccount.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+# apim
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/apim
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-apim.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+
+# linux function app
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/linux_function_app
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-linuxfunctionapp.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+# vm for vnet data gateway (to be confirmed)
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/vm
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-vm.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+
+# iot hub
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/iot_hub
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-iothub.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+# event hubs
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/event_hubs
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-eventhubs.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+
+# data explorer
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/data_explorer
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-dataexplorer.tfstate"
+
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+
+
+# stream analytics (must be last solution accelerator to be deployed)
+# ** IMPORTANT: This step requires event hubs, iot hub, data explorer and sql server to be deployed first
+cd /tf/avm/gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/stream_analytics
+
+terraform init  -reconfigure \
+-backend-config="resource_group_name=${RG_NAME}" \
+-backend-config="storage_account_name=${STG_NAME}" \
+-backend-config="container_name=2-solution-accelerators" \
+-backend-config="key=solution_accelerators-project-streamanalytics.tfstate"
+
+terraform plan \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+terraform apply -auto-approve \
+-var="storage_account_name=${STG_NAME}" \
+-var="resource_group_name=${RG_NAME}"
+
+# # Approved via Azure CLI
+# az network private-endpoint-connection approve \
+#   --id /subscriptions/{subscription-id}/resourceGroups/{resource-group}/providers/Microsoft.Devices/IotHubs/{iot-hub-name}/privateEndpointConnections/{private-connection-name} \
+#   --description "Approving private endpoint for Stream Analytics"
+
