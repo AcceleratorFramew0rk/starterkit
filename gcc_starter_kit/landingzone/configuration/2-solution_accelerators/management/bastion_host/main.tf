a61c195f3bf329b474fb3afc76c25e5c59ae6eaa
thiamsoontan <thiamsoontan@microsoft.com>
2025-02-14 09:13:53 +0800

    Update main.tf

--- gcc_starter_kit/landingzone/configuration/2-solution_accelerators/management/bastion_host/main.tf
+++ gcc_starter_kit/landingzone/configuration/2-solution_accelerators/management/bastion_host/main.tf
@@ -9,48 +9,6 @@ module "public_ip" {
   sku = "Standard"
 }
 
-
-# module "azure_bastion" {
-#   source = "../../"
-
-#   enable_telemetry    = true
-#   name                = module.naming.bastion_host.name_unique
-#   resource_group_name = try(local.global_settings.resource_group_name, null) == null ? azurerm_resource_group.this.0.name : local.global_settings.resource_group_name
-#   location            = try(local.global_settings.resource_group_name, null) == null ? azurerm_resource_group.this.0.location : local.global_settings.location
-#   copy_paste_enabled  = true
-#   file_copy_enabled   = false
-#   sku                 = "Standard"
-#   ip_configuration = {
-#     name                 = "my-ipconfig"
-#     subnet_id            = module.virtualnetwork.subnets[var.subnet_name].resource_id
-#     public_ip_address_id = azurerm_public_ip.example.id
-#   }
-#   ip_connect_enabled     = true
-#   scale_units            = 4
-#   shareable_link_enabled = true
-#   tunneling_enabled      = true
-#   kerberos_enabled       = true
-
-#   tags = {
-#     environment = "production"
-#   }
-# }
-
-# locals {
-#   tags = {
-#     scenario = "windows_w_data_disk_and_public_ip"
-#   }
-#   regions = ["southeastasia", "southeastasia"]
-
-#   source_image_reference = {
-#     publisher = "MicrosoftWindowsServer"
-#     offer     = "WindowsServer"
-#     sku       = "2022-datacenter-g2"
-#     version   = "latest"
-#   }
-# }
-
-
 module "azure_bastion" {
   source  = "Azure/avm-res-network-bastionhost/azurerm"
   version = "0.2.0"
@@ -59,7 +17,6 @@ module "azure_bastion" {
   enable_telemetry     = true
   resource_group_name  = try(local.global_settings.resource_group_name, null) == null ? azurerm_resource_group.this.0.name : local.global_settings.resource_group_name
   location             = try(local.global_settings.resource_group_name, null) == null ? azurerm_resource_group.this.0.location : local.global_settings.location  
-  # virtual_network_name = try(local.remote.networking.virtual_networks.spoke_management.virtual_network.name, null) != null ? local.remote.networking.virtual_networks.spoke_management.virtual_network.name : var.vnet_name  
 
   // Define the bastion host configuration
   name                = "${module.naming.bastion_host.name}${random_string.this.result}" 
@@ -67,9 +24,9 @@ module "azure_bastion" {
   file_copy_enabled   = false
   sku                 = "Standard" # "Premium" # for session recording preview
   ip_configuration    = {
-      name                 = "${module.naming.bastion_host.name}ipconfig" # "bhipconfig" 
+      name                 = "${module.naming.bastion_host.name}ipconfig" 
       subnet_id            = try(local.remote.networking.virtual_networks.spoke_management.virtual_subnets[var.subnet_name].resource.id, null) != null ? local.remote.networking.virtual_networks.spoke_management.virtual_subnets[var.subnet_name].resource.id : var.subnet_id 
-      public_ip_address_id = module.public_ip.public_ip_id # azurerm_public_ip.example.id
+      public_ip_address_id = module.public_ip.public_ip_id 
   }
   ip_connect_enabled     = true
   scale_units            = 2
@@ -99,11 +56,6 @@ module "azure_bastion" {
       metric_categories                        = ["AllMetrics"]
       log_analytics_destination_type           = "Dedicated"
       workspace_resource_id                    = try(local.remote.log_analytics_workspace.id, null) != null ? local.remote.log_analytics_workspace.id : var.log_analytics_workspace_id 
-
-      # storage_account_resource_id              = "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Storage/storageAccounts/{storageAccountName}"
-      # event_hub_authorization_rule_resource_id = "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.EventHub/namespaces/{namespaceName}/eventhubs/{eventHubName}/authorizationrules/{authorizationRuleName}"
-      # event_hub_name                           = "{eventHubName}"
-      # marketplace_partner_resource_id          = "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{partnerResourceProvider}/{partnerResourceType}/{partnerResourceName}"
     }
   }
 
