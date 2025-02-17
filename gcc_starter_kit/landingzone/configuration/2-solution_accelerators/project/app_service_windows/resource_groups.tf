f7734313b1bc2fe6597ddfe90b0fd5398f84b3e7
thiamsoontan <thiamsoontan@microsoft.com>
2025-02-17 13:31:21 +0800

    remove duplicate code for app service intranet and windows

--- gcc_starter_kit/landingzone/configuration/2-solution_accelerators/project/app_service_windows/resource_groups.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "this" {
-  count = try(local.global_settings.resource_group_name, null) == null ? 1 : 0
-  
-  name     = "${module.naming.resource_group.name}-solution-accelerators-appservice" 
-  location = "${try(local.global_settings.location, var.location)}" 
-}
-
-
